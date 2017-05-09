698a24ebad4ab9b27dd5feb2a73e90981b184a8f
18019675002 <591961691@qq.com>
2017-05-10 00:56:52 +0800

    。。。。。。。。。。。

--- src/main/java/com/ssm/utils/package-info.java
+++ src/main/java/com/ssm/utils/package-info.java
@@ -4,7 +4,7 @@
 /** @ClassName: package-info
  * @Description: 
  * @author Wangjie
- * @date ����ʱ�䣺2017��5��10�� ����12:48:46
+ * @date 2017年5月10日 12:48:46
  * @version 1.0
  */
 package com.ssm.utils;
